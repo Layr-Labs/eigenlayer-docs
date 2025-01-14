6413a7aa083ece38d1ef88c5739e10ff0858ec1c
Wes Floyd <wesfloyd@gmail.com>
2025-01-14 14:43:07 -0500

    modifying npm to js code

--- static/js/userback.js
+++ static/js/userback.js
@@ -1,4 +1,6 @@
-
-import Userback from "@userback/widget";
-
-Userback("A-punLdUrqzlaudld0XR5gHVAi2", options);
+window.Userback = window.Userback || {};
+  Userback.access_token = 'A-punLdUrqzlaudld0XR5gHVAi2';
+  
+  (function(d) {
+    var s = d.createElement('script');s.async = true;s.src = 'https://static.userback.io/widget/v1.js';(d.head || d.body).appendChild(s);
+  })(document);
