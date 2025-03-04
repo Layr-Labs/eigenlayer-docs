387d6ec0f6cb342155a6b6c0059ca15e8cfc821a
MadelineAu <40311741+MadelineAu@users.noreply.github.com>
2025-03-04 12:23:11 +1000

    Keys concept (#439)
    Co-authored-by: Wes Floyd <wesfloyd@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,7 +79,7 @@ const config = {
           },
           {
             from: "/operators/operator-guides/key-management/intro-key-management",
-            to: "/operators/concepts/intro-key-management",
+            to: "/eigenlayer/concepts/keys-and-signatures",
           },
           {
             from: "/operators/operator-guides/operator-introduction",
@@ -100,6 +100,10 @@ const config = {
           {
             from: "/operators/operator-guides/operator-content-guidelines",
             to: "/operators/howto/operator-content-guidelines",
+          },
+          {
+            from: "/operators/concepts/intro-key-management",
+            to: "/eigenlayer/concepts/keys-and-signatures",
           },
           {
             from: "/operators/operator-guides/operator-installation",
